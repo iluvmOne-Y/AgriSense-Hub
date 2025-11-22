ed85159a0ee2a7e0514e3181655dffbb2309b073
Livens <dangkhoah77@gmail.com>
2025-11-22 16:23:32 +0700

    [feat] initialize package.json for Agrisense Hub web application with scripts and devDependencies

---
+++ Web/.prettierrc.js
@@ -0,0 +1,29 @@
+export default {
+	// Formatting rules
+	printWidth: 80, // Maximum line length before wrapping
+
+	// Indentation settings
+	tabWidth: 4, // Number of spaces per indentation level
+	useTabs: true, // Use tabs instead of spaces
+
+	// Quote & Semicolon rules
+	semi: false, // No semicolons at the end of statements
+	singleQuote: true, // Use single quotes instead of double quotes
+	quoteProps: 'as-needed', // Only add quotes around object properties when required
+
+	// Trailing comma settings
+	trailingComma: 'es5', // Add trailing commas where valid in ES5 (e.g., objects, arrays)
+
+	// Spacing & Bracket rules
+	bracketSpacing: true, // Add spaces between brackets in object literals
+	bracketSameLine: false, // Put closing brackets of JSX elements on a new line
+
+	// Arrow function formatting
+	arrowParens: 'always', // Omit parentheses when possible in arrow functions
+
+	// Prose wrapping behavior
+	proseWrap: 'always', // Always wrap markdown text
+
+	// Line ending settings
+	endOfLine: 'lf', // Use LF (Line Feed) as the line ending
+}
---
+++ Web/eslint.config.mjs
@@ -0,0 +1,53 @@
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+import pluginReact from 'eslint-plugin-react'
+import prettierConfig from 'eslint-config-prettier'
+
+export default defineConfig(
+	{
+		ignores: ['dist', 'node_modules'],
+	},
+
+	// Base configuration for JavaScript and TypeScript files
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	},
+
+	// TypeScript ESLint configurations
+	tseslint.configs.base,
+	...tseslint.configs.recommended,
+
+	// React Hooks configurations
+	reactHooks.configs.flat.recommended,
+
+	// React configurations
+	{
+		files: ['**/*.{ts,tsx,jsx}'],
+		...pluginReact.configs.flat.recommended,
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+	},
+
+	// Specific rule adjustments
+	{
+		rules: {
+			eqeqeq: 'off',
+			'@typescript-eslint/no-namespace': 'off',
+			'@typescript-eslint/no-explicit-any': 'warn',
+		},
+	},
+
+	// Prettier configuration
+	prettierConfig
+)
