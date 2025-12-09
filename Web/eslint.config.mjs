0ba9d7b220b7797affb4f02f1aa3ff803c5d4a73
Livens <dangkhoah77@gmail.com>
2025-12-10 05:33:59 +0700

    [refactor] improve formatting and consistency in ESLint configuration

--- Web/eslint.config.mjs
+++ Web/eslint.config.mjs
@@ -6,48 +6,49 @@ import pluginReact from 'eslint-plugin-react'
 import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig(
-	{
-		ignores: ['dist', 'node_modules'],
-	},
+    {
+        ignores: ['dist', 'node_modules'],
+    },
 
-	// Base configuration for JavaScript and TypeScript files
-	{
-		files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-		},
-	},
+    // Base configuration for JavaScript and TypeScript files
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
 
-	// TypeScript ESLint configurations
-	tseslint.configs.base,
-	...tseslint.configs.recommended,
+    // TypeScript ESLint configurations
+    tseslint.configs.base,
+    ...tseslint.configs.recommended,
 
-	// React Hooks configurations
-	reactHooks.configs.flat.recommended,
+    // React Hooks configurations
+    reactHooks.configs.flat.recommended,
 
-	// React configurations
-	{
-		files: ['**/*.{ts,tsx,jsx}'],
-		...pluginReact.configs.flat.recommended,
-		settings: {
-			react: {
-				version: 'detect',
-			},
-		},
-	},
+    // React configurations
+    {
+        files: ['**/*.{ts,tsx,jsx}'],
+        ...pluginReact.configs.flat.recommended,
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
 
-	// Specific rule adjustments
-	{
-		rules: {
-			eqeqeq: 'off',
-			'@typescript-eslint/no-namespace': 'off',
-			'@typescript-eslint/no-explicit-any': 'warn',
-		},
-	},
+    // Specific rule adjustments
+    {
+        rules: {
+            eqeqeq: 'off',
+            '@typescript-eslint/unused-vars': 'warn',
+            '@typescript-eslint/no-namespace': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+        },
+    },
 
-	// Prettier configuration
-	prettierConfig
+    // Prettier configuration
+    prettierConfig
 )
