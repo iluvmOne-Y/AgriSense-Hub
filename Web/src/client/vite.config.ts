93619032689508582e2b6a4d134095cd238d1953
Livens <dangkhoah77@gmail.com>
2025-12-11 22:51:24 +0700

    [feat] allow . and .. for vite server

--- Web/src/client/vite.config.ts
+++ Web/src/client/vite.config.ts
@@ -4,37 +4,42 @@ import path from 'path'
 import dotenv from 'dotenv'
 
 export default defineConfig(() => {
-    // Load the environment files into a single object
-    const env = {
-        // ...dotenv.config({
-        //     path: path.resolve(__dirname, '../shared/shared.env'),
-        // }).parsed,
-        ...dotenv.config({ path: path.resolve(__dirname, 'client.env') })
-            .parsed,
-    }
+	// Load the environment files into a single object
+	const env = {
+		// ...dotenv.config({
+		//     path: path.resolve(__dirname, '../shared/shared.env'),
+		// }).parsed,
+		...dotenv.config({ path: path.resolve(__dirname, 'client.env') })
+			.parsed,
+	}
 
-    // Exposes the variables to your client-side code via `import.meta.env`
-    const envWithMeta = Object.entries(env).reduce((prev, [key, val]) => {
-        return {
-            ...prev,
-            [`import.meta.env.${key}`]: JSON.stringify(val),
-        }
-    }, {})
+	// Exposes the variables to your client-side code via `import.meta.env`
+	const envWithMeta = Object.entries(env).reduce((prev, [key, val]) => {
+		return {
+			...prev,
+			[`import.meta.env.${key}`]: JSON.stringify(val),
+		}
+	}, {})
 
-    return {
-        plugins: [
-            react({
-                babel: {
-                    plugins: [['babel-plugin-react-compiler']],
-                },
-            }),
-        ],
-        resolve: {
-            alias: {
-                Client: path.resolve(__dirname, './src'),
-                Shared: path.resolve(__dirname, '../shared/src'),
-            },
-        },
-        define: envWithMeta,
-    }
+	return {
+		plugins: [
+			react({
+				babel: {
+					plugins: [['babel-plugin-react-compiler']],
+				},
+			}),
+		],
+		resolve: {
+			alias: {
+				Client: path.resolve(__dirname, './src'),
+				Shared: path.resolve(__dirname, '../shared/src'),
+			},
+		},
+		server: {
+			fs: {
+				allow: ['.', '..'],
+			},
+		},
+		define: envWithMeta,
+	}
 })
